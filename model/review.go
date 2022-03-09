f2649ccdd015ad3e67e4a07e98e2a61185052cb6
ymsht <ymshttts@gmail.com>
2022-03-09 21:59:50 +0900

    [update]int64のmarshal追加

--- model/review.go
+++ model/review.go
@@ -12,32 +12,40 @@ type NullString struct {
 	sql.NullString
 }
 
+type NullInt64 struct {
+	sql.NullInt64
+}
+
 // Review レビュー情報
 type Review struct {
-	ReviewID     int           `db:"review_id" json:"review_id"`
-	MemberId     int           `db:"member_id" json:"member_id"`
-	DrinkingDay  sql.NullTime  `db:"drinking_day" json:"drinking_day"`
-	IsPublic     bool          `db:"is_public" json:"is_public"`
-	Brewery      NullString    `db:"brewery" json:"brewery"`
-	Beer_name    string        `db:"beer_name" json:"beer_name"`
-	Store        NullString    `db:"store" json:"store"`
-	Bar          NullString    `db:"bar" json:"bar"`
-	Aroma        sql.NullInt64 `db:"aroma" json:"aroma"`
-	BitterTaste  sql.NullInt64 `db:"bitterTaste" json:"bitterTaste"`
-	SweetTaste   sql.NullInt64 `db:"sweetTaste" json:"sweetTaste"`
-	Body         sql.NullInt64 `db:"body" json:"body"`
-	Sharpness    sql.NullInt64 `db:"sharpness" json:"sharpness"`
-	CountryId    sql.NullInt64 `db:"country_id" json:"country_id"`
-	Memo         NullString    `db:"memo" json:"memo"`
-	Create_date  time.Time     `db:"create_date" json:"create_date"`
-	Update_date  time.Time     `db:"update_date" json:"update_date"`
-	Evaluation   sql.NullInt64 `db:"evaluation" json:"evaluation"`
-	StyleId      sql.NullInt64 `db:"style_id" json:"style_id"`
-	PurchaseDate sql.NullTime  `db:"purchase_date" json:"purchase_date"`
+	ReviewID     int          `db:"review_id" json:"review_id"`
+	MemberId     int          `db:"member_id" json:"member_id"`
+	DrinkingDay  sql.NullTime `db:"drinking_day" json:"drinking_day"`
+	IsPublic     bool         `db:"is_public" json:"is_public"`
+	Brewery      NullString   `db:"brewery" json:"brewery"`
+	Beer_name    string       `db:"beer_name" json:"beer_name"`
+	Store        NullString   `db:"store" json:"store"`
+	Bar          NullString   `db:"bar" json:"bar"`
+	Aroma        NullInt64    `db:"aroma" json:"aroma"`
+	BitterTaste  NullInt64    `db:"bitterTaste" json:"bitterTaste"`
+	SweetTaste   NullInt64    `db:"sweetTaste" json:"sweetTaste"`
+	Body         NullInt64    `db:"body" json:"body"`
+	Sharpness    NullInt64    `db:"sharpness" json:"sharpness"`
+	CountryId    NullInt64    `db:"country_id" json:"country_id"`
+	Memo         NullString   `db:"memo" json:"memo"`
+	Create_date  time.Time    `db:"create_date" json:"create_date"`
+	Update_date  time.Time    `db:"update_date" json:"update_date"`
+	Evaluation   NullInt64    `db:"evaluation" json:"evaluation"`
+	StyleId      NullInt64    `db:"style_id" json:"style_id"`
+	PurchaseDate sql.NullTime `db:"purchase_date" json:"purchase_date"`
 }
 
 func (s NullString) MarshalJSON() ([]byte, error) {
 	return json.Marshal(s.String)
+}
+
+func (i NullInt64) MarshalJSON() ([]byte, error) {
+	return json.Marshal(i.Int64)
 }
 
 func (s *NullString) UnmarshalJSON(data []byte) error {
@@ -48,6 +56,17 @@ func (s *NullString) UnmarshalJSON(data []byte) error {
 	}
 	s.String = str
 	s.Valid = str != ""
+	return nil
+}
+
+func (i *NullInt64) UnmarshalJSON(data []byte) error {
+	var intger int64
+	err := json.Unmarshal(data, &intger)
+	if err != nil {
+		return err
+	}
+	i.Int64 = intger
+	i.Valid = intger != 0
 	return nil
 }
 
