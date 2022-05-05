5d9f6284713eddd48d718b2ac708a60fdf884bda
ymsht <ymshttts@gmail.com>
2022-05-05 14:23:24 +0900

    ログ追加

--- api/review.go
+++ api/review.go
@@ -16,6 +16,7 @@ func GetReviews() echo.HandlerFunc {
 		tx := c.Get("Tx").(*gorp.Transaction)
 		r, err := model.GetReviews(tx)
 		if err != nil {
+			c.Logger().Error(err.Error())
 			return c.JSON(http.StatusInternalServerError, err)
 		}
 
@@ -31,6 +32,7 @@ func GetReview() echo.HandlerFunc {
 		id, _ := strconv.Atoi(id_str)
 		r, err := model.GetReview(tx, id)
 		if err != nil {
+			c.Logger().Error(err.Error())
 			return c.JSON(http.StatusInternalServerError, err)
 		}
 
